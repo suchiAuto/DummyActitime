b4f6bab9079f84126e41fec3d1b6b3de7983f177
HP <HP@DESKTOP-5LFKS0F>
2020-09-14 06:10:56 +0530

    This is my firstcommit on 14Sep20

---
+++ SeleniumDummyCodeForTEAMCITY/src/com/act/login/LoginToApp.java
@@ -0,0 +1,42 @@
+package com.act.login;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+public class LoginToApp {
+
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System .setProperty("webdriver.chrome.driver", "D:\\chromedriver_win32\\chromedriver.exe");
+//		System .setProperty("webdriver.ie.driver", "D:\\IEDriverServer_x64_3.150.12\\IEDriverServer.exe");
+//		WebDriver driver=new InternetExplorerDriver();
+		
+		
+		WebDriver driver=new ChromeDriver();
+		driver.get("http://localhost/login.do");
+		driver.findElement(By.id("username")).sendKeys("admin");
+		driver.findElement(By.name("pwd")).sendKeys("manager");
+		driver.findElement(By.id("loginButton")).click();
+		
+		driver.manage().window().maximize();
+	boolean flg=driver.findElement(By.xpath("//td[@class='logoCell navCell']")).isDisplayed();
+	if(flg==true)
+	{
+		System.out.println("Logo Displayed");
+	}
+	else
+	{
+		System.out.println("LOgo Not Displayed");
+	}
+	//Assert.assertTrue(flg);
+	
+		
+		
+
+	}
+
+}
